b94ebf3008cbf73c5fdeef9ccb5c9f76bdbb4545
ItsCetrion <goodplay2.0@mail.ru>
2024-06-02 20:26:55 +0700

    Модификаторы доступа Registration
    Прописал модификаторы доступа для Controller у Registration

--- TransportCompany/Controller/ControllerRegistration.py
+++ TransportCompany/Controller/ControllerRegistration.py
@@ -11,114 +11,115 @@ import hashlib
 
 class ControllerRegister:
     def __init__(self):
-        self.model = ModelRegistration()
-        self.view = ViewRigistration()
-        self.SettingsUI()
-        self.view.pushButton_SignUp.clicked.connect(self.functionsSignUp)
-        self.view.pushButton_Back.clicked.connect(self.functionsBack)
-        self.view.lineEdit_FirstName.textChanged.connect(self.ColorGrenFirstName)
-        self.view.lineEdit_LastName.textChanged.connect(self.ColorGrenLastName)
-        self.view.lineEdit_Patronymic.textChanged.connect(self.ColorGrenPatronymic)
-        self.view.lineEdit_NumberPhone.textChanged.connect(self.CheckNumderPhone)
-        self.view.lineEdit_Email.textChanged.connect(self.CheckEmail)
-        self.view.lineEdit_Password.textChanged.connect(self.CheckPassword)
-        self.view.lineEdit_AgainPassword.textChanged.connect(self.CheckAgainPassword)
+        self.__model = ModelRegistration()
+        self.__view = ViewRigistration()
+        self.__SettingsUI()
 
-    def SettingsUI(self):
-        self.app = QtWidgets.QApplication(sys.argv)
-        self.RegisterWindow = QtWidgets.QMainWindow()
-        ui = self.view
-        ui.setupUi(self.RegisterWindow)
+        self.__view.pushButton_SignUp.clicked.connect(self.__functionsSignUp)
+        self.__view.pushButton_Back.clicked.connect(self.__functionsBack)
+        self.__view.lineEdit_FirstName.textChanged.connect(self.__ColorGrenFirstName)
+        self.__view.lineEdit_LastName.textChanged.connect(self.__ColorGrenLastName)
+        self.__view.lineEdit_Patronymic.textChanged.connect(self.__ColorGrenPatronymic)
+        self.__view.lineEdit_NumberPhone.textChanged.connect(self.__CheckNumderPhone)
+        self.__view.lineEdit_Email.textChanged.connect(self.__CheckEmail)
+        self.__view.lineEdit_Password.textChanged.connect(self.__CheckPassword)
+        self.__view.lineEdit_AgainPassword.textChanged.connect(self.__CheckAgainPassword)
+
+    def __SettingsUI(self):
+        self.__app = QtWidgets.QApplication(sys.argv)
+        self.__RegisterWindow = QtWidgets.QMainWindow()
+        ui = self.__view
+        ui.setupUi(self.__RegisterWindow)
 
     def RunViewRegister(self):
-        self.RegisterWindow.show()
+        self.__RegisterWindow.show()
 
-    def ColorGrenFirstName(self):
-        self.view.lineEdit_FirstName.setStyleSheet("background-color: rgb(0, 255, 127);"
+    def __ColorGrenFirstName(self):
+        self.__view.lineEdit_FirstName.setStyleSheet("background-color: rgb(0, 255, 127);"
                                                    "border-radius: 10px")
 
-    def ColorGrenLastName(self):
-        self.view.lineEdit_LastName.setStyleSheet("background-color: rgb(0, 255, 127);"
+    def __ColorGrenLastName(self):
+        self.__view.lineEdit_LastName.setStyleSheet("background-color: rgb(0, 255, 127);"
                                                   "border-radius: 10px")
 
-    def ColorGrenPatronymic(self):
-        self.view.lineEdit_Patronymic.setStyleSheet("background-color: rgb(0, 255, 127);"
+    def __ColorGrenPatronymic(self):
+        self.__view.lineEdit_Patronymic.setStyleSheet("background-color: rgb(0, 255, 127);"
                                                     "border-radius: 10px")
 
-    def CheckNumderPhone(self):
-        if self.model.CheckPhone(self.view.lineEdit_NumberPhone.text()) or len(self.view.lineEdit_NumberPhone.text()) < 11:
-            self.view.lineEdit_NumberPhone.setStyleSheet("background-color: rgb(170, 0, 0);"
+    def __CheckNumderPhone(self):
+        if self.__model.CheckPhone(self.__view.lineEdit_NumberPhone.text()) or len(self.__view.lineEdit_NumberPhone.text()) < 11:
+            self.__view.lineEdit_NumberPhone.setStyleSheet("background-color: rgb(170, 0, 0);"
                                                     "border-radius: 10px")
         else:
-            self.view.lineEdit_NumberPhone.setStyleSheet("background-color: rgb(0, 255, 127);"
+            self.__view.lineEdit_NumberPhone.setStyleSheet("background-color: rgb(0, 255, 127);"
                                                     "border-radius: 10px")
 
-    def CheckEmail(self):
+    def __CheckEmail(self):
         try:
-            email = self.view.lineEdit_Email.text()
+            email = self.__view.lineEdit_Email.text()
             validate_email(email, check_deliverability=False)
         except EmailNotValidError:
-            self.view.lineEdit_Email.setStyleSheet("background-color: rgb(170, 0, 0);"
+            self.__view.lineEdit_Email.setStyleSheet("background-color: rgb(170, 0, 0);"
                                                     "border-radius: 10px")
         else:
-            if self.model.CheckEmail(email):
-                self.view.lineEdit_Email.setStyleSheet("background-color: rgb(170, 0, 0);"
+            if self.__model.CheckEmail(email):
+                self.__view.lineEdit_Email.setStyleSheet("background-color: rgb(170, 0, 0);"
                                                         "border-radius: 10px")
             else:
-                self.view.lineEdit_Email.setStyleSheet("background-color: rgb(0, 255, 127);"
+                self.__view.lineEdit_Email.setStyleSheet("background-color: rgb(0, 255, 127);"
                                                         "border-radius: 10px")
 
-    def CheckPassword(self):
-        password = self.view.lineEdit_Password.text()
+    def __CheckPassword(self):
+        password = self.__view.lineEdit_Password.text()
         if len(password) == 0:
-            self.view.lineEdit_Password.setStyleSheet("background-color: rgb(170, 0, 0);"
+            self.__view.lineEdit_Password.setStyleSheet("background-color: rgb(170, 0, 0);"
                                                  "border-radius: 10px")
         else:
             stats = PasswordStats(password)
             if 0 <= stats.strength() <= 0.2:
-                self.view.lineEdit_Password.setStyleSheet("background-color: rgb(170, 0, 0);"
+                self.__view.lineEdit_Password.setStyleSheet("background-color: rgb(170, 0, 0);"
                                                      "border-radius: 10px")
             elif 0.2 < stats.strength() <= 0.66:
-                self.view.lineEdit_Password.setStyleSheet("background-color: rgb(255, 170, 0);"
+                self.__view.lineEdit_Password.setStyleSheet("background-color: rgb(255, 170, 0);"
                                                      "border-radius: 10px")
             elif 0.66 < stats.strength() <= 0.99:
-                self.view.lineEdit_Password.setStyleSheet("background-color: rgb(0, 255, 127);"
+                self.__view.lineEdit_Password.setStyleSheet("background-color: rgb(0, 255, 127);"
                                                      "border-radius: 10px")
 
-    def CheckAgainPassword(self):
-        if self.view.lineEdit_AgainPassword.text() != self.view.lineEdit_Password.text():
-            self.view.lineEdit_AgainPassword.setStyleSheet("background-color: rgb(170, 0, 0);"
+    def __CheckAgainPassword(self):
+        if self.__view.lineEdit_AgainPassword.text() != self.__view.lineEdit_Password.text():
+            self.__view.lineEdit_AgainPassword.setStyleSheet("background-color: rgb(170, 0, 0);"
                                                       "border-radius: 10px")
         else:
-            self.view.lineEdit_AgainPassword.setStyleSheet("background-color: rgb(0, 255, 127);"
+            self.__view.lineEdit_AgainPassword.setStyleSheet("background-color: rgb(0, 255, 127);"
                                                       "border-radius: 10px")
 
-    def functionsSignUp(self):
-        if self.CheckColor():
+    def __functionsSignUp(self):
+        if self.__CheckColor():
             user = Client()
-            user.FirstName = self.view.lineEdit_FirstName.text()
-            user.LastName = self.view.lineEdit_LastName.text()
-            user.Patronymic = self.view.lineEdit_Patronymic.text()
-            user.NumberPhone = self.view.lineEdit_NumberPhone.text()
-            user.Email = self.view.lineEdit_Email.text()
-            password = self.view.lineEdit_Password.text()
+            user.FirstName = self.__view.lineEdit_FirstName.text()
+            user.LastName = self.__view.lineEdit_LastName.text()
+            user.Patronymic = self.__view.lineEdit_Patronymic.text()
+            user.NumberPhone = self.__view.lineEdit_NumberPhone.text()
+            user.Email = self.__view.lineEdit_Email.text()
+            password = self.__view.lineEdit_Password.text()
             user.Password = hashlib.md5(password.encode()).hexdigest()
-            self.model.UserRigistation(user)
-            self.view.message("Информация", "Успешно")
-            self.functionsBack()
+            self.__model.UserRigistation(user)
+            self.__view.message("Информация", "Успешно")
+            self.__functionsBack()
         else:
-            self.view.message("Информация", "Не все поля заполнены корректно или вовсе незаполненны")
+            self.__view.message("Информация", "Не все поля заполнены корректно или вовсе незаполненны")
 
-    def CheckColor(self):
-        for LineEdit in self.RegisterWindow.findChildren(QtWidgets.QLineEdit):
+    def __CheckColor(self):
+        for LineEdit in self.__RegisterWindow.findChildren(QtWidgets.QLineEdit):
             hexcolor = LineEdit.palette().color(LineEdit.backgroundRole()).name()
             if hexcolor == "#aa0000" or hexcolor == "#ffffff" or len(LineEdit.text()) < 2:
                 return False
         else:
             return True
 
-    def functionsBack(self):
+    def __functionsBack(self):
         from TransportCompany.Controller.ControllerLogin import ControllerLogin
         self.ControllerLogin = ControllerLogin()
-        self.RegisterWindow.close()
+        self.__RegisterWindow.close()
         self.ControllerLogin.RunViewLogin()
